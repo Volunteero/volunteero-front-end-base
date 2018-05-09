cee9a2af8f7069fa69fd4c8409fd4f326a4b085a
Jack D <dmarkov23@gmail.com>
2018-05-09 13:37:24 +0200

    setup of routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,9 +2,16 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from '../components/home/home.component';
 import {RouteNotFoundComponent} from '../components/route-not-found/route-not-found.component';
+import {DiscoverComponent} from '../components/discover/discover.component';
+import {CreateComponent} from '../components/create/create.component';
+import {UserProfileComponent} from '../components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'discover', component: DiscoverComponent},
+  {path: 'create', component: CreateComponent},
+  {path: 'profile', component: UserProfileComponent},
+
   {path: '**', component: RouteNotFoundComponent}
 ];
 
