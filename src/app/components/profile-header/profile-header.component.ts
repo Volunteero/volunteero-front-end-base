175cc025d715a90f9e88510295789da3227057f7
dmitrii <dmitry-orlov@hotmail.com>
2018-06-15 23:32:31 +0200

    removed an oupsie like contact name set to 'string'

--- src/app/components/profile-header/profile-header.component.ts
+++ src/app/components/profile-header/profile-header.component.ts
@@ -12,7 +12,7 @@ export class ProfileHeaderComponent implements OnInit {
     displayName: '',
     location: '',
     points: 0,
-    contactName: 'string',
+    contactName: '',
   };
 
   @Input()
