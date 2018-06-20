c6215d546469b47723320d5201e0e3976dbda465
AtanasNaydenov <atanasnaydenov@outlook.com>
2018-06-20 20:52:59 +0200

    user and role update
    public user and level added to Role.ts

--- src/app/models/Role.ts
+++ src/app/models/Role.ts
@@ -14,14 +14,14 @@ export interface Role {
   id?: string;
   // displayName: string;
   title?: string;
-  // level: string;
+  level: string;
   // location: string;
   // imageUrl: string;
   accessToken?: string;
 }
 
 export class SimpleRole implements Role {
-  // level: string;
+  level: string;
   // location: string;
   // imageUrl: string;
   accessToken: string;
